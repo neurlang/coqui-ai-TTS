0105024fb607c71a4bcf49d68b6c0971a9100612
neurlang <77860779+neurlang@users.noreply.github.com>
2025-08-08 08:17:27 +0200

    Update __init__.py
    type package name here

--- TTS/__init__.py
+++ TTS/__init__.py
@@ -2,7 +2,7 @@ import importlib.metadata
 
 from TTS.utils.generic_utils import is_pytorch_at_least_2_4
 
-__version__ = importlib.metadata.version("coqui-tts-version")
+__version__ = importlib.metadata.version("coqui-tts-pygoruut")
 
 if "coqpit" in importlib.metadata.packages_distributions().get("coqpit", []):
     msg = (
